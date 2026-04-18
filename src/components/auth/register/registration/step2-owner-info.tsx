b0b8ce53fed863a8372d4121a5c37add92980e56
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2026-04-18 18:26:44 +0530

    updated
    updated

--- src/components/auth/register/registration/step2-owner-info.tsx
+++ src/components/auth/register/registration/step2-owner-info.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRegistration } from "@/lib/register/registration-context";
